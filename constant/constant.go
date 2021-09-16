ab41ee9fada1c3d3aa6000aa6e443e7339dfba5f
Sahril Mahendra <sahrilmahendra@gmail.com>
2021-09-16 21:46:47 +0700

    menambahkan file constant.go

---
+++ constant/constant.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main(){
+	// constant isinya tidak dapat diubah
+	// berbeda dengan variable, constant tidak akan error meskipun tidak digunakan
+	const (
+		firstName = "Sahril"
+		lastName = "Mahendra"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(firstName, lastName)
+}
