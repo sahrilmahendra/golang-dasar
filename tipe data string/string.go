6c0e41cb770bdd80a9c7ad8025d3524d4aeb5de6
Sahril Mahendra <sahrilmahendra@gmail.com>
2021-09-16 21:17:58 +0700

    menambahkan file string.go

---
+++ tipe data string/string.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Moch.")
+	fmt.Println("Syahryil")
+	fmt.Println("Mahendra")
+	fmt.Println("Moch. Syahryil Mahendra")
+	fmt.Println(len("Moch."))
+	fmt.Println(len("Syahryil"))
+	fmt.Println(len("Mahendra"))
+	fmt.Println(len("Moch. Syahryil Mahendra"))
+}
