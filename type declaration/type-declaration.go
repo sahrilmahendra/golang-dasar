faade2b537c21ff9a6515a270d98bc9c8c29067e
Sahril Mahendra <sahrilmahendra@gmail.com>
2021-09-17 10:44:00 +0700

    menambahkan file type-declaration.go

---
+++ type declaration/type-declaration.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	type noKTP string
+	type lulus bool
+	var KTPSahril noKTP = "00190827349"
+	var statusSahril lulus = true
+	fmt.Println(KTPSahril)
+	fmt.Println(statusSahril)
+}
