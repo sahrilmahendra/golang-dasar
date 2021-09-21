8319de6f4ce1fab3060f59ba96c387090fb9d587
Sahril Mahendra <sahrilmahendra@gmail.com>
2021-09-21 19:13:21 +0700

    menambahkan isi helper.go dan import.go

--- helper/helper.go
+++ helper/helper.go
@@ -2,6 +2,13 @@ package helper
 
 import "fmt"
 
+var version = 1.3
+var Application = "Belajar Golang"
+
 func SayHello(name string) {
 	fmt.Println("Hello", name)
+}
+
+func sayGoodBye(name string) {
+	fmt.Println("Good Bye", name)
 }
--- import/import.go
+++ import/import.go
@@ -1,7 +1,13 @@
 package main
 
-import "golang-dasar/helper"
+import (
+	"fmt"
+	"golang-dasar/helper"
+)
 
 func main() {
 	helper.SayHello("Sahril")
+	// helper.sayGoodBye("Sahril") // error
+	fmt.Println(helper.Application)
+	// fmt.Println(helper.version) // error
 }
