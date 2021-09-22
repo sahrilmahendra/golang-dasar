bb2003ce9a18d78fee934598da5e3b17f8c2fa8b
Sahril Mahendra <sahrilmahendra@gmail.com>
2021-09-22 09:53:33 +0700

    menambahkan container-ring.go

---
+++ container-ring/container-ring.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"container/ring"
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var data *ring.Ring = ring.New(5)
+
+	for i := 0; i < data.Len(); i++ {
+		data.Value = "Data " + strconv.FormatInt(int64(i), 10)
+		data = data.Next()
+	}
+	data.Do(func(i interface{}) {
+		fmt.Println(i)
+	})
+}
